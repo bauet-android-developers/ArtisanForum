ca8112bbe2a09480db4d8b9f5a47a9d2a8038061
ERROR <fsadakathussin@gmail.com>
2019-06-27 23:12:05 +0600

    3rd commit

--- app/src/main/java/com/example/artisanforum/MainActivity.java
+++ app/src/main/java/com/example/artisanforum/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.artisanforum;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        x.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
