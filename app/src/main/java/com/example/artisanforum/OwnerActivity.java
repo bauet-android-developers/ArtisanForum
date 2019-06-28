1a0832bf7d6edb77c20742064d9e401d667a67af
ERROR <fsadakathussin@gmail.com>
2019-06-28 17:35:46 +0600

    Owner and Customer Activity added++

--- app/src/main/java/com/example/artisanforum/OwnerActivity.java
+++ app/src/main/java/com/example/artisanforum/OwnerActivity.java
@@ -2,6 +2,8 @@ package com.example.artisanforum;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class OwnerActivity extends AppCompatActivity {
 
