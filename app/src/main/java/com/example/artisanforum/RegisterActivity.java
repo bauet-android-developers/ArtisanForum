e367e874fece9ad22e49a730b51f901622126923
ERROR <fsadakathussin@gmail.com>
2019-06-28 21:53:29 +0600

    hbjb

--- app/src/main/java/com/example/artisanforum/RegisterActivity.java
+++ app/src/main/java/com/example/artisanforum/RegisterActivity.java
@@ -105,7 +105,7 @@ public class RegisterActivity extends AppCompatActivity {
                                 public void onComplete(@NonNull Task<Void> task) {
                                     if (task.isSuccessful()){
                                         pd.dismiss();
-                                        Intent intent = new Intent(RegisterActivity.this, DonorEditProfileActivity.class);
+                                        Intent intent = new Intent(RegisterActivity.this, CustomerActivity.class);
                                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                                         startActivity(intent);
                                     }
