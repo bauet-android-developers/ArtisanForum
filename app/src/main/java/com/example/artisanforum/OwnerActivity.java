fc0b7866297f8b9c9958d9e3d7147dedf126542c
ERROR <fsadakathussin@gmail.com>
2019-06-28 17:22:57 +0600

    Owner and Customer Activity added+

--- app/src/main/java/com/example/artisanforum/OwnerActivity.java
+++ app/src/main/java/com/example/artisanforum/OwnerActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 
 public class OwnerActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner);
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //banaan tree
     }
 }
